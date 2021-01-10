b11faf605d73492451e6d2ff8879f01b5f5ceeab
Ryo Manzoku <manzoku.ryo@gmail.com>
2021-01-10 12:23:33 +0900

    fix test for invitecode

--- invitecode_test.go
+++ invitecode_test.go
@@ -5,28 +5,50 @@ import (
 )
 
 func TestGetAddressFromInviteCode(t *testing.T) {
-	is := initializeTest(t)
 	if testInviteCode == "" {
-		t.Skip()
+		t.Skip("test invite code is not found")
 	}
 	ok, address, err := GetAddressFromInviteCode(testInviteCode)
-	is.Nil(err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	if !ok {
-		t.Errorf("ok is false")
+		t.Fatal("ok is false")
 	}
 	if address != testAddress {
-		t.Errorf("got %v\nwant %v", address, testAddress)
+		t.Fatalf("got %v\nwant %v", address, testAddress)
 	}
 }
 
 func TestGetInviteCodeFromAddress(t *testing.T) {
-	is := initializeTest(t)
-	if testAddress == "" {
-		t.Skip()
+
+	type args struct {
+		address string
 	}
-	inviteCode, err := GetInviteCodeFromAddress(testAddress)
-	is.Nil(err)
-	if inviteCode != testInviteCode {
-		t.Errorf("got %v\nwant %v", inviteCode, testInviteCode)
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				"0xd868711BD9a2C6F1548F5f4737f71DA67d821090",
+			},
+			want:    "THA4",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, err := GetInviteCodeFromAddress(tt.args.address)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetInviteCodeFromAddress() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if code != tt.want {
+				t.Errorf("GetInviteCodeFromAddress() code = %v, wantErr %v", code, tt.want)
+			}
+		})
 	}
 }
